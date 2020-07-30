83ef051b2754268af4752b13b7bd7925d74b1640
ymaloth <yakuswamy123@gmail.com>
2020-07-30 16:26:35 +1000

    addition and static files create

--- server.js
+++ server.js
@@ -6,9 +6,15 @@ let app2=express()
 app.use(express.static('public'))
 
 
-/*app.get('/',function(request,response){
-    response.send('Hello world')
-})*/
+let addition=function(num1, num2){
+    let result=num1+num2;
+    return result;
+}
+
+app.get('/addition',function(request,response){
+    let myResult=addition(10,9);
+    response.send('Addition= '+myResult);
+});
 
 // start the server on port 3000
 app.listen(3000)
